a8fe4444814d31b578a56acbeb2a0a9086d3cd99
Savitha M <savitha@Savithas-MacBook-Air.local>
2023-11-18 16:28:27 +0530

    module-esports

---
+++ math.js
@@ -0,0 +1,8 @@
+const s=(a,b)=>a+b;
+const g=(a,b)=>a*b;
+const pi=3.14;
+let obj={
+    s:s,
+    g:g
+}
+module.exports=obj;
---
+++ script.js
@@ -0,0 +1,11 @@
+// let n=5;
+// console.log("hello..",process.argv)
+// let x=process.argv;
+// for(i=2;i<x.length;i++){
+//     console.log("hi",x[i]);
+// }
+// const somevalue=require('./math')
+// console.log(somevalue.g(1,2));
+
+const fruits=require("./fruits");
+console.log(fruits);
